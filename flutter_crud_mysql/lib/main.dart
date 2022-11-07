c29a77e6320b51331092ab2549d6d77bc55085b8
EDISAPUTRA\LENOVO <edisaputra@unja.ac.id>
2022-11-07 11:21:53 +0700

    lib main

---
+++ flutter_crud_mysql/lib/main.dart
@@ -0,0 +1,97 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  Future<List<dynamic>> getData() async {
+    final response =
+        await http.get(Uri.parse("http://10.0.2.2/my_store/getalldata.php"));
+    return json.decode(response.body);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("MY STORE"),
+        ),
+        body: FutureBuilder<List>(
+          future: getData(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return const SizedBox.shrink(); //<---here
+            } else {
+              if (snapshot.hasData) {
+                return ItemList(list: snapshot.data!);
+              } else {
+                return const Center(child: CircularProgressIndicator());
+              }
+            }
+          },
+        ));
+  }
+}
+
+class ItemList extends StatelessWidget {
+  //const ItemList({super.key});
+  final List<dynamic> list;
+  const ItemList({super.key, required this.list});
+
+  @override
+  Widget build(BuildContext context) {
+    if (list != null) {
+      return ListView.builder(
+        itemCount: list.length,
+        itemBuilder: (context, index) {
+          return Card(
+              child: ListTile(
+            title: Text(
+              list[index]['item_name'],
+            ),
+            subtitle: Text("harga\t:" + list[index]['price']),
+            trailing: Text("stok\t:" + list[index]['stock']),
+          ));
+        },
+      );
+    } else {
+      return Text(" No Data");
+    }
+  }
+}
